bad21309902fea7b13c6abd5c64243e832f4b3a0
Liu Ziyi <liuziyi@Lius-MacBook-Pro.local>
2016-09-29 15:29:34 +0800

    edit secret key

--- mvp_dj/settings.py
+++ mvp_dj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$riq%1zr_c1grou(h#qk=$yji!sx02x)746bt1_b6bzosvstr7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
